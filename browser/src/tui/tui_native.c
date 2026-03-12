800ae519a976b86d5611d1bdfb24532a6fefc4c8
mizchi <miz404@gmail.com>
2026-03-12 23:54:43 +0900

    Wire browser repaint loop and pointer hit testing

--- browser/src/tui/tui_native.c
+++ browser/src/tui/tui_native.c
@@ -37,7 +37,7 @@ int tui_enable_raw_mode(void) {
   raw.c_cflag |= CS8;
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   raw.c_cc[VMIN] = 0;
-  raw.c_cc[VTIME] = 1;
+  raw.c_cc[VTIME] = 0;
   if (tcsetattr(fd, TCSADRAIN, &raw) == -1) return -1;
   raw_mode_enabled = 1;
   return 0;
